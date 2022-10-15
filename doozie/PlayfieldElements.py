9c8c6b408af34c90dbfdf22a594ec9023cb62d3f
Adam Fletcher <adamgingernut@googlemail.com>
2022-10-15 23:06:50 +0100

    add playfield elements

---
+++ doozie/PlayfieldElements.py
@@ -0,0 +1,31 @@
+class PlayfieldElement():
+    def __init__(self, config_dict):
+        self.type = config_dict['type']
+        self.name = config_dict['name']
+        self.led_color = 'White'
+        self.light_address = None
+        self.switch_pin = None
+        self.relay_pin = None
+        self.points_lit = config_dict['points_lit']
+        self.points_unlit = config_dict['points_unlit']
+        self.is_lit = config_dict['starts_lit']
+
+    def score_points(self):
+        #TODO: Add a tally to score points
+        return self.points_lit if self.is_lit else self.points_unlit
+
+    def hit_effect(self):
+        return None
+
+class PopBumper(PlayfieldElement):
+    def __init__(self, config_dict):
+        super().__init__(config_dict)
+        self.physical_colour = config_dict['color']
+
+class SlingShot(PlayfieldElement):
+    def __init__(self, config_dict):
+        super().__init__(config_dict)
+
+class Switch(PlayfieldElement):
+    def __init__(self, config_dict):
+        super().__init__(config_dict)
--- doozie/doozie_classic.py
+++ doozie/doozie_classic.py
@@ -1,17 +1,9 @@
 import yaml
 
-class PlayfieldElement():
-    ...
-
-class PopBumper():
-    def __init__(self, config_dict):
-        self.colour = config_dict['color']
-        self.points_lit = config_dict['points_lit']
-        self.points_unlit = config_dict['points_unlit']
-        self.is_lit = False
-        self.relay_pin = None
-        self.light_pin = None
-
+try:
+    from PlayfieldElements import PopBumper, SlingShot, Switch
+except:
+    from doozie.PlayfieldElements import PopBumper, SlingShot, Switch
 
 class Doozie():
     def __init__(self, yaml_config):
@@ -20,13 +12,51 @@ class Doozie():
                 self.my_dict = yaml.safe_load(stream)
             except yaml.YAMLError as exc:
                 print(exc)
+        self._init_pop_bumpers()
+        self._init_slingshots()
+        self._init_switches()
 
+    def _init_pop_bumpers(self):
         self.TopLeftPop = PopBumper(self.my_dict['PopBumpers']['TopLeft'])
+        self.TopRightPop = PopBumper(self.my_dict['PopBumpers']['TopRight'])
+        self.CenterPop = PopBumper(self.my_dict['PopBumpers']['Center'])
+        self.BottomLeftPop = PopBumper(self.my_dict['PopBumpers']['BottomLeft'])
+        self.BottomRightPop = PopBumper(self.my_dict['PopBumpers']['BottomRight'])
 
+    def _init_slingshots(self):
+        self.LeftSlingshot = SlingShot(self.my_dict['Slingshots']['Left'])
+        self.RightSlingshot = SlingShot(self.my_dict['Slingshots']['Right'])
 
-    def thing(self):
-        ...
+    def _init_switches(self):
+        #todo: define hit effects\
+        #Target Switches
+        self.GateTarget = Switch(self.my_dict['Switches']['Gate'])
+        self.LeftOpenZipper = Switch(self.my_dict['Switches']['LeftOpenZipper'])
+        self.LeftCloseZipper = Switch(self.my_dict['Switches']['LeftCloseZipper'])
+        self.RightOpenZipper = Switch(self.my_dict['Switches']['RightOpenZipper'])
+        self.RightCloseZipper = Switch(self.my_dict['Switches']['RightCloseZipper'])
+        #Playfield Rollover Switches
+        self.OneRollover = Switch(self.my_dict['Switches']['OnePlayfieldRollover'])
+        self.TwoRollover = Switch(self.my_dict['Switches']['TwoPlayfieldRollover'])
+        self.ThreeRollover = Switch(self.my_dict['Switches']['ThreePlayfieldRollover'])
+        self.FourRollover = Switch(self.my_dict['Switches']['FourPlayfieldRollover'])
+        self.FiveRollover = Switch(self.my_dict['Switches']['FivePlayfieldRollover'])
+        self.FiveRolloverTop = Switch(self.my_dict['Switches']['FivePlayfieldRolloverTop'])
+        #Lane Rollerover Switches
+        self.OneLaneSwitch = Switch(self.my_dict['Switches']['OneLaneRollover'])
+        self.TwoLaneSwitch = Switch(self.my_dict['Switches']['TwoLaneRollover'])
+        self.ThreeLaneSwitch = Switch(self.my_dict['Switches']['ThreeLaneRollover'])
+        self.FourLaneSwitch = Switch(self.my_dict['Switches']['FourLaneRollover'])
+        #Special Switches
+        self.LeftOutlaneSwitch = Switch(self.my_dict['Switches']['LeftOutlaneSwitch'])
+        self.RightOutlaneSwitch = Switch(self.my_dict['Switches']['RightOutlaneSwitch'])
+        self.ShooterLaneSpecialSwitch = Switch(self.my_dict['Switches']['ShooterLaneSwitch'])
+
+    #Todo: List the things
+    # Add the bumpers
+    # Add the non-scoring sections
+    #
+
 
 if __name__ == "__main__":
     doozie = Doozie('doozie_classic_config.yaml')
-    print(doozie.TopLeftPop.points_lit)
